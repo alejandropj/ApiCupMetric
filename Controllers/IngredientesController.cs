a558db4af284bdb99b5af7ee32d909aee8aebaed
alejandropj <alejandroparrajimenez@gmail.com>
2024-04-16 20:29:30 +0200

    First controller + Works!

---
+++ Controllers/IngredientesController.cs
@@ -0,0 +1,31 @@
+﻿using ApiCupMetric.Models;
+using ApiCupMetric.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCupMetric.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IngredientesController : ControllerBase
+    {
+        private RepositoryIngredientes repo;
+        public IngredientesController (RepositoryIngredientes repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Ingrediente>>> 
+            GetIngredientes() 
+        {
+            return await this.repo.GetIngredientesAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Ingrediente>>
+            FindIngrediente(int id)
+        {
+            return await this.repo.FindIngredienteByIdAsync(id);
+        }
+    }
+}
---
+++ Models/RecetaIngredienteValoracion.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiCupMetric.Models
+{
+    public class RecetaIngredienteValoracion
+    {
+        public List<int> IdIngrediente { get; set; }
+        public List<string> NombreIngrediente { get; set; }
+        public List<int> Cantidad { get; set; }
+        public List<bool> Medible { get; set; }
+
+        public Receta Receta { get; set; }
+        public int Valoracion { get; set; }
+    }
+}
